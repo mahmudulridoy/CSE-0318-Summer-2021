73f79ee550f8e6ad25dfe5822a34d2be3d389887
Mahmudul Islam <86741162+mahmudulridoy@users.noreply.github.com>
2021-09-23 01:47:19 +0600

    Add files via upload

---
+++ Final Report, Presentation and Code of Project/Code File/config.php
@@ -0,0 +1,16 @@
+<?php
+
+@header('Location: install');
+exit();
+// MySQL Hostname
+$sql_db_host = "db_name";
+// MySQL Database User
+$sql_db_user = "db_user";
+// MySQL Database Password
+$sql_db_pass = "db_pass";
+// MySQL Database Name
+$sql_db_name = "db_name";
+
+// Site URL
+$site_url = "site_url"; // e.g (http://example.com)
+?>
---
+++ Final Report, Presentation and Code of Project/Code File/expire_pro.php
@@ -0,0 +1,4 @@
+<?php 
+
+require_once('assets/init.php');
+Wo_DeleteProMemebership();
